4fa765002145cb026b0e6ed1d52f090d94f6eaed
Jose Moreno <morenojr90@yahoo.com>
2018-06-15 15:05:36 -0700

    add INVISIBLE back to button

--- app/src/main/java/me/josefmoreno/liveordie/ui/StoryActivity.java
+++ app/src/main/java/me/josefmoreno/liveordie/ui/StoryActivity.java
@@ -61,7 +61,7 @@ public class StoryActivity extends AppCompatActivity {
       storyTextView.setText(pageText);
 
       if (page.isFinalPage()) {
-        choice1Button.setVisibility(View.);
+        choice1Button.setVisibility(View.INVISIBLE);
         choice2Button.setText(R.string.play_again_button_text);
       }
       else {
