b052efc57192555addf5b5378e1ce1ce013c91e4
Jose Moreno <morenojr90@yahoo.com>
2018-06-15 13:26:58 -0700

    'makingSure'

--- app/src/main/java/me/josefmoreno/liveordie/ui/StoryActivity.java
+++ app/src/main/java/me/josefmoreno/liveordie/ui/StoryActivity.java
@@ -1,16 +1,29 @@
 package me.josefmoreno.liveordie.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import me.josefmoreno.liveordie.R;
 import me.josefmoreno.liveordie.model.Page;
+import me.josefmoreno.liveordie.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+    private String name;
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
 
     @Override
@@ -18,13 +31,52 @@ public class StoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if (name == null || name.isEmpty()) {
             name = "Friend";
         }
         Log.d(TAG, name);
 
+        story = new Story();
+        loadPage(0);
+
+
+    }
+
+    private void loadPage(int pageNumber) {
+      final Page page = story.getPage(pageNumber);
+
+      Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+      storyImageView.setImageDrawable(image);
+
+      String pageText = getString(page.getTextId());
+      // Add name if placeholder include. Wont add if not
+      pageText = String.format(pageText, name);
+      storyTextView.setText(pageText);
+
+      choice1Button.setText(page.getChoice1().getTextId());
+      choice1Button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              int nextPage = page.getChoice1().getNextPage();
+              loadPage(nextPage);
+          }
+      });
+
+      choice2Button.setText(page.getChoice2().getTextId());
+      choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
 
     }
 }
